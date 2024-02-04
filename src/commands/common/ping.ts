8835d08bab3c8f42dc17e799d420cdcc9d7a24bb
MatheusCarvalho12 <mendoncacarvalhomatheus@gmail.com>
2024-02-04 04:19:28 -0300

    Test Commands, Events and Components Handler

---
+++ src/commands/common/ping.ts
@@ -0,0 +1,17 @@
+import {
+  ActionRowBuilder,
+  ApplicationCommandType,
+  ButtonBuilder,
+  ButtonStyle,
+  Collection,
+} from "discord.js";
+import { Command } from "../../structs/types/Command";
+
+export default new Command({
+  name: "ping",
+  description: "reply with pong",
+  type: ApplicationCommandType.ChatInput,
+  async run({ interaction }) {
+    interaction.reply({ ephemeral: true, content: "pong" });
+  },
+});
